d7d3f494091aa4c8d5b7756c2593752415089b39
mateuszkwarciak <mateuszkwarciakk@gmail.com>
2019-05-12 23:46:24 +0200

    add handling javaFX

--- src/main/java/mk/app/procesy/ProcesyApplication.java
+++ src/main/java/mk/app/procesy/ProcesyApplication.java
@@ -2,12 +2,38 @@ package mk.app.procesy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import mk.app.procesy.configuration.AppConfig;
 
 @SpringBootApplication
-public class ProcesyApplication {
+public class ProcesyApplication extends Application {
 
+	private static final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProcesyApplication.class, args);
+		
+		launch(args);
 	}
+	
+	@SuppressWarnings("static-access")
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		//TODO: not working, 
+		loader.setControllerFactory(applicationContext::getBean);
+		
+		Parent root = loader.load(getClass().getResource("/fxml/Main.fxml"));
+		Scene scene = new Scene(root);
 
+		primaryStage.setTitle("Procesy");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
