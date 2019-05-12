98ef472fb6f36a0d3dba2eb2be620b108f93eaa3
mateuszkwarciak <mateuszkwarciakk@gmail.com>
2019-05-12 12:22:09 +0200

    add spring framework

---
+++ src/main/java/mk/app/procesy/ProcesyApplication.java
@@ -0,0 +1,14 @@
+package mk.app.procesy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesyApplication.class, args);
+		int a = 3;
+	}
+
+}
