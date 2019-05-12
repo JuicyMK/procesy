83c29e18646d6dcacdef4bafd81fad7f387f1727
mateuszkwarciak <mateuszkwarciakk@gmail.com>
2019-05-12 23:45:33 +0200

    first configuration

--- src/main/java/mk/app/procesy/configuration/AppConfig.java
+++ src/main/java/mk/app/procesy/configuration/AppConfig.java
@@ -1,5 +1,11 @@
 package mk.app.procesy.configuration;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("mk.app.procesy")
 public class AppConfig {
+
 
 }
