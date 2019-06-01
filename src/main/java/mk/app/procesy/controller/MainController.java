91db6cbe6e55e568de693288bd91a96a38d52bbc
mateuszkwarciak <mateuszkwarciakk@gmail.com>
2019-06-01 16:36:07 +0200

    add handling chart

--- src/main/java/mk/app/procesy/controller/MainController.java
+++ src/main/java/mk/app/procesy/controller/MainController.java
@@ -1,23 +1,63 @@
 package mk.app.procesy.controller;
 
 import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import com.jfoenix.controls.JFXButton;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 import mk.app.procesy.io.Reader;
+import mk.app.procesy.math.HeatProcessor;
+import mk.app.procesy.math.Interpolation;
+import mk.app.procesy.model.DataSet;
 
 @Slf4j
-public class MainController {
+public class MainController implements Initializable{
 
 	@FXML private JFXButton selectFileBtn;
 	@FXML private JFXButton computeBtn;
 	@FXML private JFXButton saveFileBtn1;
+    @FXML private AnchorPane rightAnchor;
+    @FXML private ScrollPane stepsSP;
+    
+	private LineChart<Number, Number> chart;
+    
+	private DataSet data;
+	
+	@SuppressWarnings("static-access")
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		NumberAxis xAxis = new NumberAxis();
+		NumberAxis yAxis = new NumberAxis();
+		
+		xAxis.setLabel("H");
+		yAxis.setLabel("Temperatura");
+		
+		chart = new LineChart<Number, Number>(xAxis, yAxis);
+		chart.setTitle("Ciepło");
+		
+		rightAnchor.getChildren().add(chart);
+		rightAnchor.setTopAnchor(chart, 10.0);
+		rightAnchor.setLeftAnchor(chart, 10.0);
+		rightAnchor.setBottomAnchor(chart, 10.0);
+		rightAnchor.setRightAnchor(chart, 10.0);
+	}
 
 	@FXML
 	void readData(ActionEvent event) {
@@ -30,13 +70,35 @@ public class MainController {
 		if (selectedFile != null) {
 			reader.setFile(selectedFile);
 			selectFileBtn.setText(selectedFile.getName());
-			//TODO: handle parsed data
-			reader.parse();
+
+			prepareAndSaveDatas(reader);
+			updateChart();
+			
 			log.debug("Selected file {}", selectedFile.getName());
 		} else {
 			reader.setFile(null);
+			data = null;
+			updateChart();
+			
 			selectFileBtn.setText("Wczytaj dane");
 			log.warn("File not found");
+		}
+	}
+
+	private void prepareAndSaveDatas(Reader reader) {
+		List<Pair<Integer, Double>> inputData = reader.parse();
+		
+		inputData = Interpolation.interpol(inputData);
+		
+		List<Integer> tmp = inputData.stream().map(e -> e.getLeft()).collect(Collectors.toList());
+		List<Double> computeH = HeatProcessor.compute(inputData);
+		
+		try {
+			data = new DataSet(tmp ,computeH);
+			log.info("Dane do przetwarzania zostały poprawnie zapisane");
+		} catch (Exception e) {
+			data = null;
+			log.error("Błąd łączenia danych: {}", e);
 		}
 	}
 
@@ -49,4 +111,19 @@ public class MainController {
     void saveFile(ActionEvent event) {
 
     }
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private void updateChart() {
+		log.debug("Aktualizuję wykres, tmp i h: ", data.getNewTmpAndH());
+		if (data != null) {
+			XYChart.Series<Number, Number> orginalData = new XYChart.Series<Number, Number>();
+			orginalData.setName("Wczytane dane");
+
+			for (Pair<Integer, Double> row : data.getOrgTmpAndH()) {
+				orginalData.getData().add(new XYChart.Data(row.getLeft(), row.getRight()));
+			}
+
+			chart.getData().add(orginalData);
+		} 
+	}
 }
