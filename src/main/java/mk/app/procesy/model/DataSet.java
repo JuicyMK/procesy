e120be7de9db37dd5aa83f3df86bcdb935bd1fb9
mateuszkwarciak <mateuszkwarciakk@gmail.com>
2019-06-01 19:42:47 +0200

    add parameterized controller

--- src/main/java/mk/app/procesy/model/DataSet.java
+++ src/main/java/mk/app/procesy/model/DataSet.java
@@ -20,10 +20,14 @@ public class DataSet {
 	
 	private List<Pair<Integer, Double>> orgTmpAndH;
 	private List<Pair<Integer, Double>> newTmpAndH;
+	
+	private List<ModificationStep> steps; 
 
 	public DataSet(List<Integer> tmp, List<Double> h) throws Exception {
 		orgTmpAndH = new ArrayList<>();
 		newTmpAndH = new ArrayList<>();
+		steps = new ArrayList<>();
+		
 		if (tmp.size() != h.size()) {
 			log.error("Lista tmp nie pokrywa się z listą H");
 			throw new Exception("Niewłaściwe rozmiary list tmp i h");
