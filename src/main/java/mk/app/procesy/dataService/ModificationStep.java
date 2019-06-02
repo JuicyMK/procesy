93a146b5c5439f724b9a060375cc6bfb9643d6dc
mateuszkwarciak <mateuszkwarciakk@gmail.com>
2019-06-02 14:13:53 +0200

    add from, to and valueH field

--- src/main/java/mk/app/procesy/dataService/ModificationStep.java
+++ src/main/java/mk/app/procesy/dataService/ModificationStep.java
@@ -4,26 +4,43 @@ import java.util.List;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public abstract class ModificationStep {
 	
 	protected int from;
 	protected int to;
+	protected double valueH;
 	
-	public abstract void modify(List<Pair<Integer, Double>> tmpAndH, double valueH);
+	public abstract List<Double> modify(List<Pair<Integer, Double>> tmpAndH);
 	
-	public void setFrom(int from) {
-		this.from = from;
+	protected Pair<Integer, Integer> lookForScope(List<Pair<Integer, Double>> tmpAndH) {
+		Integer firstIdx = null;
+		Integer lastIdx = null; 
+		
+		for (int i = 0; i <tmpAndH.size(); i++) {
+			if (tmpAndH.get(i).getLeft().compareTo(from) == 0) {
+				firstIdx = i;
+				log.trace("EvenlyStep:lookForScope firstIdx : {}", firstIdx);
+			} else if (tmpAndH.get(i).getLeft().compareTo(to) == 0) {
+				lastIdx = i;
+				log.trace("EvenlyStep:lookForScope lastIdx : {}", lastIdx);
+				break;
+			}
+		}
+		return Pair.of(firstIdx, lastIdx);
 	}
-
+	
 	public int getFrom() {
 		return from;
 	}
 
-	public void setTo(int to) {
-		this.to = to;
-	}
-
 	public int getTo() {
 		return to;
+	}
+	
+	public double getValueH() {
+		return valueH;
 	}
 }
