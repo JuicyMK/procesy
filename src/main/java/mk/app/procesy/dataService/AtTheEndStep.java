a748c9f304adb83b31835fa9748595f1bc655223
mateuszkwarciak <mateuszkwarciakk@gmail.com>
2019-06-02 11:56:19 +0200

    add atTheEndStep logic

--- src/main/java/mk/app/procesy/dataService/AtTheEndStep.java
+++ src/main/java/mk/app/procesy/dataService/AtTheEndStep.java
@@ -4,12 +4,21 @@ import java.util.List;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class AtTheEndStep extends ModificationStep {
 
-	@Override
-	public void modify(List<Pair<Integer, Double>> tmpAndH, int from, int to) {
-		// TODO Auto-generated method stub
-
+	public void modify(List<Pair<Integer, Double>> tmpAndH, double valueH) {
+		log.debug("AtTheEndStep: wyszykuje wartość do aktualizacji");
+		for (Pair<Integer, Double> row : tmpAndH) {
+			if (row.getLeft().compareTo(to) == 0) {
+				double h = row.getRight() + valueH;
+				row = Pair.of(to, h);
+				log.debug("AtTheEndStep: Dla tmp: {}, zwiększam H do {}", row.getLeft(), h);
+				break;
+			}
+		}
 	}
 
 }
