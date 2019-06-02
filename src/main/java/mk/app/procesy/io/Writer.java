751b9823e850ae82a4cc3e71f508a099962234f6
mateuszkwarciak <mateuszkwarciakk@gmail.com>
2019-06-02 21:36:48 +0200

    add file writter

---
+++ src/main/java/mk/app/procesy/io/Writer.java
@@ -0,0 +1,50 @@
+package mk.app.procesy.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.springframework.util.CollectionUtils;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import mk.app.procesy.dataService.DataSet;
+
+@Slf4j
+public class Writer {
+
+	private static Writer instance;
+	
+	private static final String OUTPUT_FORMAT = "%-10s %-10s \n";
+	
+	public static synchronized Writer getInstance() {
+		if (instance == null) {
+			instance = new Writer();
+		}
+		
+		return instance;
+	}
+	
+	public boolean saveFile(File file, DataSet dataSet) {
+		try {
+			PrintWriter writer = new PrintWriter(file);
+			writer.println("Wyniki działania programu:");
+			writer.printf(OUTPUT_FORMAT, "Tmp", "H");
+			
+			if(dataSet != null && !CollectionUtils.isEmpty(dataSet.getNewTmpAndH())) {
+				dataSet.getNewTmpAndH().stream().forEachOrdered(e -> {
+					writer.printf(OUTPUT_FORMAT, e.getLeft(), e.getRight());
+				});
+			}
+			
+			writer.close();
+			log.info("Dane pomyślnie zapisano do pliku");
+			return true;
+		} catch (IOException ex) {
+			log.error("Błąd zapisu do pliku");
+			return false;
+		}
+	}
+
+}
